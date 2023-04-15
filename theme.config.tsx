4d8c131148437f2ca88e0644a876432299f035b8
linard-y <130826947+MoOmgaia@users.noreply.github.com>
2023-04-15 10:07:19 +0200

    Initiation by cloning local repository
    Initiation by cloning local repository

---
+++ theme.config.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {DocsThemeConfig} from "nextra-theme-docs";
+import {useRouter} from "next/router";
+import GiscusComments from "./components/giscus/giscus";
+import BackToTop from "./components/backToTop/backToTop";
+
+
+const config: DocsThemeConfig = {
+        project: {
+            link: "https://github.com/LuciferUchiha/nextra-garden"
+        },
+        docsRepositoryBase: "https://github.com/LuciferUchiha/nextra-garden/tree/main/",
+        primaryHue: 180,
+        nextThemes: {
+            defaultTheme: "dark"
+        },
+        editLink: {
+            text: "Edit this page on GitHub →"
+        },
+        feedback: {
+            content: null // disable because now using Giscus
+        },
+        useNextSeoProps() {
+            const {asPath} = useRouter();
+            return {titleTemplate: (asPath !== "/") ? "%s – GR" : "georgerowlands.ch"};
+        },
+        head: <link rel="icon" type="image/x-icon" href="logo.svg"/>,
+        logo: (<>
+              <img src="/logo.svg" width="64" height="48" alt="" />
+              <span className="ml-4 font-bold">Mô'Omgaïa</span>
+              </>),
+        search: {
+            // TODO I can replace the component that is shown for when nothing was found with an image of a dead tree
+            placeholder: "recherche d'un article..."
+        },
+        sidebar: {defaultMenuCollapseLevel: 1},
+        toc: {
+            title: "Table of contents"
+        },
+        main: ({children}) => {
+            return (
+                <>
+                    {children}
+                    <BackToTop/>
+                    <GiscusComments/>
+                </>
+            );
+        },
+        footer: {
+            text: () =>
+                <div className="flex w-full flex-row items-center justify-between">
+                    <ul>
+                        <li><p className="font-bold">Socials</p></li>
+                        <li><a href="https://www.linkedin.com/in/georgerowlands/" className="text-sm">Linkedin↗</a></li>
+                        <li><a href="https://github.com/LuciferUchiha" className="text-sm">GitHub↗</a></li>
+                        <li><a href="https://stackoverflow.com/users/10994912/george-r"
+                               className="text-sm">StackOverflow↗</a></li>
+                    </ul>
+                    <a
+
+                        href="https://www.buymeacoffee.com/georgerowlands" target="_blank"
+                    >
+                        <img
+                            src="buyMeACoffeePurple.png"
+                            alt="Buy Me A Coffee"
+                            style={{height: "41px", width: "174px"}}
+                        />
+                    </a>
+                </div>
+        }
+    }
+;
+
+export default config;
