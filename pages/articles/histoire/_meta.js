83ee2fcfebaa3810b2a137bd931edc83eb76a09f
moomgaia <moomgai@free.fr>
2026-06-07 16:10:57 +0200

    reecriture sans accents

--- pages/articles/histoire/_meta.js
+++ pages/articles/histoire/_meta.js
@@ -1,7 +1,7 @@
 const meta_histoire = {
   origines: "Les Origines",
   etnographie: "Histoire etnographique",
-  société: "Sociétés et organisations",
+  societe: "Sociétés et organisations",
 };
 
 export default meta_histoire;
