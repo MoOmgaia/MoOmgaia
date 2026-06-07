620ce4278a9e9e5525315ddb59ec9ca9aeb98842
moomgaia <moomgai@free.fr>
2026-06-07 10:26:42 +0200

    add sub-menu "société" to menu "histoire"

--- pages/articles/histoire/_meta.js
+++ pages/articles/histoire/_meta.js
@@ -1,6 +1,7 @@
 const meta_histoire = {
   origines: "Les Origines",
-  etnographie: "Histoire etnographique"
+  etnographie: "Histoire etnographique",
+  société: "Sociétés et organisations",
 };
 
 export default meta_histoire;
